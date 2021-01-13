75f7621448c3acb734edb7844f45759a53385090
chukwumaokere <chukwumaokere@yahoo.com>
2021-01-13 00:47:27 -0600

    changing color of skeleton for consistency with ui

--- src/components/ChatSkeletonRight.vue
+++ src/components/ChatSkeletonRight.vue
@@ -3,12 +3,12 @@
       <div class="max-w-sm">
         <div class="animate-pulse flex space-x-4">
             <div class="flex-1 space-y-4 py-1">
-                <div class="h-4 bg-gray-400 rounded w-3/4"></div>
+                <div class="h-4 bg-blue-400 rounded w-3/4"></div>
                 <div class="space-y-2">
-                    <div class="h-4 bg-gray-400 rounded"></div>
+                    <div class="h-4 bg-blue-400 rounded"></div>
                 </div>
             </div>
-             <div class="rounded-full bg-gray-400 h-12 w-12"></div>
+             <div class="rounded-full bg-blue-400 h-12 w-12"></div>
         </div>
       </div>
     </div>
