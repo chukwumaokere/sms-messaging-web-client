52b91b268c94f4c41dd53f9f75b1ff3694d989b8
chukwumaokere <chukwumaokere@yahoo.com>
2021-01-13 13:27:33 -0600

    adding other interaction ui elements

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,12 +12,12 @@
                   @keyup="updateSearchValue"
                 />
         <div class="w-6 float-right absolute right-8 ">
-            <button v-if="!searchOpen" @click="openSearch" class="p-0 w-7 h-7 bg-transparent ripple rounded-lg object-cover mouse transition ease-in duration-200 focus:outline-none text-gray-400 hover:text-gray-700 dark:hover:text-white">
+            <button v-if="!searchOpen" @click="openSearch" class="p-0 w-7 h-7 bg-transparent ripple rounded-lg object-cover mouse transition ease-in duration-200 focus:outline-none text-gray-400 hover:text-gray-700 dark:hover:text-white ">
                 <svg class="w-6 h-6 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
             </button>
-            <button v-if="searchOpen" @click="openSearch" class="p-0 pt-2 w-7 h-7 bg-transparent ripple rounded-lg object-cover mouse transition ease-in duration-200 focus:outline-none text-gray-400 hover:text-gray-700 dark:hover:text-white">
+            <button v-if="searchOpen" @click="openSearch" class="p-0 pt-2 w-7 h-7 bg-transparent ripple rounded-lg object-cover mouse transition ease-in duration-200 focus:outline-none text-gray-400 hover:text-gray-700 dark:hover:text-white ">
                 <svg class="w-6 h-6 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
@@ -66,7 +66,7 @@
     <div class="flex options w-full mx-auto px-10 absolute bottom-0 pb-5 p-5" id="options">
         <ul class="flex space-x-4 mx-auto">
             <li>
-                <button @click="toggleDarkMode" class="flex items-center focus:outline-none justify-center text-gray-400 hover:text-gray-700 dark:hover:text-indigo-100 dark:text-indigo-500 h-10 w-10 transition ease-in">
+                <button @click="toggleDarkMode" class="flex items-center focus:outline-none justify-center text-gray-400 hover:text-gray-700 dark:hover:text-indigo-100 dark:text-indigo-500 h-10 w-10 transition ease-in rounded hover:bg-white hover:bg-opacity-10">
                 <svg class="w-6 h-6"
                     fill="none"
                     stroke="currentColor"
@@ -81,8 +81,8 @@
             </li>
             <li>
                 <button 
-                    class="flex items-center focus:outline-none">
-                    <span class="flex items-center focus:outline-none justify-center text-gray-400 hover:text-gray-700 dark:hover:text-indigo-100 dark:text-indigo-500 h-10 w-10 rounded-2xl transition ease-in">
+                    class="flex items-center focus:outline-none rounded hover:bg-white hover:bg-opacity-10 ">
+                    <span class="flex items-center focus:outline-none justify-center text-gray-400 hover:text-gray-700 dark:hover:text-indigo-100 dark:text-indigo-500 h-10 w-10 rounded-2xl transition ease-in ">
                         <svg class="w-6 h-6"
                             fill="none"
                             stroke="currentColor"
